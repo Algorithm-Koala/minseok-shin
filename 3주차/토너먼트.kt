61d304d20fd284e1ef7cf2e3f7860728877b0134
t1nm1ksun <122257945+t1nm1ksun@users.noreply.github.com>
2024-09-04 16:01:18 +0900

    [solve] 1057 토너먼트

---
+++ 3주차/토너먼트.kt
@@ -0,0 +1,34 @@
+package com.example.algorithm_koala
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+
+fun main() {
+    // 입력
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    // 출력
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    var (N,a,b) = br.readLine().split(" ").map { it.toInt() }
+
+    var cnt = 0
+
+    while(true){
+        a= (a+1)/2
+        b = (b+1)/2
+        cnt++
+        if (a==b){
+            break
+        }
+    }
+    bw.write(cnt.toString())
+
+    // 버퍼 비우기, 안하면 시간 초과
+    bw.flush()
+    // 스트림 닫기
+    bw.close()
+    br.close()
+}
